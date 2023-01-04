35426a8f79ee17bf21aca3f63979dd5f87997226
shusuke <shusuke.oshiro@el-ltd.co.jp>
2023-01-04 13:23:48 +0900

    First commit

---
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.its.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().frameOptions().disable();
+
+
+        http
+                .authorizeRequests()
+                .mvcMatchers("/users/**").hasAuthority("ADMIN")
+                .mvcMatchers("/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
+
+
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into issues (summary, description) values ('バグA', 'バグがあります');
+insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
+
+--password1234
+insert into users (username, password, authority) values ('tom','873d34c721039283af68109f06bae51d81a8d33db7c85a14cdf5e428cb5e2f214a7515b0b1b5d13a','ADMIN');
+insert into users (username, password, authority) values ('bob','3976169889a1da0ec50ff74d830319c073b7d98197a7686323fa538239a2c37f685251f75e9f8db6','USER');
+
